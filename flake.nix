c47ff4b4b9ebdf94317d193c9ee2e40767f98475
shwewo <shwewo@gmail.com>
2024-02-24 04:40:47 +0300

    release 23.11

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "Telegram Desktop with custom patches";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = inputs @ { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+      let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
       {
         packages = rec {
           telegram-desktop-patched = pkgs.telegram-desktop.overrideAttrs (oldAttrs: {
