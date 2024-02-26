e31079731d8d1054601e8aad2376ce3638415b34
shwewo <shwewo@gmail.com>
2024-02-26 19:04:02 +0300

    define nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Telegram Desktop with custom patches";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
